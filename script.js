12e97580d59beeec1da8183220e44268fb1842d2
Aparecido Sousa <jose.aparecido@edu.pilar.al.gov.br>
2026-07-30 23:10:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -63,7 +63,7 @@ if (selectModoGabarito) {
 // DADOS DO CABEÇALHO
 let dadosCabecalho = {
     nomeEscola: "ESCOLA MUNICIPAL DE EDUCAÇÃO BÁSICA NOSSA SENHORA DO PILAR",
-    logoUrl: "",
+    logoUrl: "/images/logopilar.png",
     tituloProva: "SIMULADO DE MATEMÁTICA - SAEB 2026",
     serie: "8º ANO",
     turma: "A",
